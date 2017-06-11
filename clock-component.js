1d69c95cce5633a572828ec47b6fb8147d0d0f1e
Anwesh43 <anweshthecool0@gmail.com>
2017-06-11 17:29:26 +0530

    adding clock class

--- clock-component.js
+++ clock-component.js
@@ -5,16 +5,55 @@ class ClockComponent extends HTMLElement {
         const shadow = this.attachShadow({mode:'open'})
         this.img = document.createElement('img')
         shadow.appendChild(this.img)
+        this.clock = new Clock()
     }
     render() {
         const canvas = document.createElement('canvas')
         canvas.width = clockSize
         canvas.height = clockSize
         const context = canvas.getContext('2d')
+        this.clock.draw(context)
         this.img.src = canvas.toDataURL()
     }
     connectedCallback() {
         this.render()
     }
 }
-customElements.define('clock',ClockComponent)
+class Clock {
+    constructor() {
+        this.getTimeAngles()
+    }
+    getTimeAngles() {
+        const date = new Date()
+        const hour = date.getHours()%12
+        const minute = date.getMinutes()
+        const seconds = date.getSeconds()
+        this.sa = seconds * 6
+        this.ma = minute * 6 + (seconds/60)*6
+        this.ha = hour * 30 + (minute/60)*30+(seconds/3600)*30
+    }
+    draw(context) {
+        context.strokeStyle = 'black'
+        context.lineWidth = clockSize/40
+        context.beginPath()
+        context.save()
+        context.translate(clockSize/2,clockSize/2)
+        context.arc(0,0,2*clockSize/5,0,2*Math.PI)
+        context.stroke()
+        for(var i=0;i<12;i++) {
+            context.save()
+            context.rotate((i+1)*Math.PI/6)
+            context.beginPath()
+            context.moveTo(0,-2*clockSize/5)
+            context.lineTo(0,-clockSize/3)
+            context.stroke()
+            const text = ""+(i+1)
+            context.font = context.font.replace(/\d{2}/,clockSize/15)
+            const tw = context.measureText(text).width
+            context.fillText(text,-tw/2,-clockSize/4)
+            context.restore()
+        }
+        context.restore()
+    }
+}
+customElements.define('clock-component',ClockComponent)
